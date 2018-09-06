5bc5a7d126e54a87c694fe24b13636a5ef54a892
Max Williams <futuresharks@gmail.com>
2018-09-06 10:40:34 +0200

    adding 3 examples

---
+++ examples/postgresql/main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_version = ">= 0.11.7"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "azs" {
+  type = "list"
+
+  default = [
+    "us-east-1a",
+    "us-east-1b",
+    "us-east-1c",
+  ]
+}
+
+module "aurora" {
+  source                          = "../../"
+  name                            = "aurora-example"
+  engine                          = "aurora-postgresql"
+  engine-version                  = "9.6.3"
+  subnets                         = ["${module.vpc.database_subnets}"]
+  availability_zones              = ["${var.azs}"]
+  vpc_id                          = "${module.vpc.vpc_id}"
+  replica_count                   = 1
+  allowed_security_groups         = ["${aws_security_group.app_servers.id}"]
+  instance_type                   = "db.r4.large"
+  apply_immediately               = true
+  skip_final_snapshot             = true
+  db_parameter_group_name         = "${aws_db_parameter_group.aurora_db_postgres96_parameter_group.id}"
+  db_cluster_parameter_group_name = "${aws_rds_cluster_parameter_group.aurora_cluster_postgres96_parameter_group.id}"
+}
+
+resource "aws_db_parameter_group" "aurora_db_postgres96_parameter_group" {
+  name        = "test-aurora-db-postgres96-parameter-group"
+  family      = "aurora-postgresql9.6"
+  description = "test-aurora-db-postgres96-parameter-group"
+}
+
+resource "aws_rds_cluster_parameter_group" "aurora_cluster_postgres96_parameter_group" {
+  name        = "test-aurora-postgres96-cluster-parameter-group"
+  family      = "aurora-postgresql9.6"
+  description = "test-aurora-postgres96-cluster-parameter-group"
+}
+
+resource "aws_security_group" "app_servers" {
+  name        = "app-servers"
+  description = "For application servers"
+  vpc_id      = "${module.vpc.vpc_id}"
+}
+
+resource "aws_security_group_rule" "allow_access" {
+  type                     = "ingress"
+  from_port                = "${module.aurora.port}"
+  to_port                  = "${module.aurora.port}"
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.app_servers.id}"
+  security_group_id        = "${module.aurora.security_group_id}"
+}
+
+module "vpc" {
+  source = "git@github.com:terraform-aws-modules/terraform-aws-vpc.git?ref=v1.37.0"
+  name   = "example"
+  cidr   = "10.0.0.0/16"
+  azs    = ["${var.azs}"]
+
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/25",
+  ]
+
+  public_subnets = [
+    "10.0.4.0/24",
+    "10.0.5.0/24",
+    "10.0.6.0/25",
+  ]
+
+  database_subnets = [
+    "10.0.7.0/24",
+    "10.0.8.0/24",
+    "10.0.9.0/25",
+  ]
+}
