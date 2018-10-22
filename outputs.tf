9b58c86f10d348bc6e858d85f8deec75166d5994
Max Williams <max.williams@deliveryhero.com>
2018-10-22 11:40:20 +0200

    Conditional outputs

--- outputs.tf
+++ outputs.tf
@@ -1,42 +1,39 @@
-// aws_rds_cluster
 output "this_rds_cluster_id" {
   description = "The ID of the cluster"
-  value       = "${aws_rds_cluster.this.id}"
+  value       = "${element(split(",", join(",", aws_rds_cluster.this.*.id)), 0)}"
 }
 
 output "this_rds_cluster_endpoint" {
   description = "The cluster endpoint"
-  value       = "${aws_rds_cluster.this.endpoint}"
+  value       = "${element(split(",", join(",", aws_rds_cluster.this.*.endpoint)), 0)}"
 }
 
 output "this_rds_cluster_reader_endpoint" {
   description = "The cluster reader endpoint"
-  value       = "${aws_rds_cluster.this.reader_endpoint}"
-}
-
-output "this_rds_cluster_master_password" {
-  description = "The master password"
-  value       = "${aws_rds_cluster.this.master_password}"
-}
-
-output "this_rds_cluster_port" {
-  description = "The port"
-  value       = "${aws_rds_cluster.this.port}"
+  value       = "${element(split(",", join(",", aws_rds_cluster.this.*.reader_endpoint)), 0)}"
 }
 
 output "this_rds_cluster_master_username" {
   description = "The master username"
-  value       = "${aws_rds_cluster.this.master_username}"
+  value       = "${element(split(",", join(",", aws_rds_cluster.this.*.master_username)), 0)}"
 }
 
-// aws_rds_cluster_instance
+output "this_rds_cluster_master_password" {
+  description = "The master password"
+  value       = "${element(split(",", join(",", aws_rds_cluster.this.*.master_password)), 0)}"
+}
+
+output "this_rds_cluster_port" {
+  description = "The port"
+  value       = "${element(split(",", join(",", aws_rds_cluster.this.*.port)), 0)}"
+}
+
 output "this_rds_cluster_instance_endpoints" {
   description = "A list of all cluster instance endpoints"
   value       = ["${aws_rds_cluster_instance.this.*.endpoint}"]
 }
 
-// aws_security_group
 output "this_security_group_id" {
   description = "The security group ID of the cluster"
-  value       = "${aws_security_group.this.id}"
+  value       = "${element(split(",", join(",", aws_security_group.this.*.id)), 0)}"
 }
