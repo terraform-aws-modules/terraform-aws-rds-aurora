515bc7cd26bf6605b7a9e096586664a71ea7bdf4
Max Williams <max.williams@deliveryhero.com>
2018-11-05 09:58:49 +0100

    Chang conditional to use &&

--- main.tf
+++ main.tf
@@ -87,14 +87,14 @@ data "aws_iam_policy_document" "monitoring_rds_assume_role" {
 }
 
 resource "aws_iam_role" "rds_enhanced_monitoring" {
-  count = "${var.create_resources ? var.monitoring_interval > 0 ? 1 : 0 : 0}"
+  count = "${var.create_resources && var.monitoring_interval > 0 ? 1 : 0}"
 
   name               = "rds-enhanced-monitoring-${var.name}"
   assume_role_policy = "${data.aws_iam_policy_document.monitoring_rds_assume_role.json}"
 }
 
 resource "aws_iam_role_policy_attachment" "rds_enhanced_monitoring" {
-  count = "${var.create_resources ? var.monitoring_interval > 0 ? 1 : 0 : 0}"
+  count = "${var.create_resources && var.monitoring_interval > 0 ? 1 : 0}"
 
   role       = "${aws_iam_role.rds_enhanced_monitoring.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
