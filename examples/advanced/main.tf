6cb5a6a0454fcff17f48522a2070cd22b5905781
Anton Babenko <anton@antonbabenko.com>
2019-06-11 19:56:29 +0200

    Upgraded module to support Terraform 0.12 (#45)

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -2,23 +2,27 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "azs" {
-  type = "list"
-
-  default = [
-    "us-east-1a",
-    "us-east-1b",
-    "us-east-1c",
-  ]
+######################################
+# Data sources to get VPC and subnets
+######################################
+data "aws_vpc" "default" {
+  default = true
 }
 
+data "aws_subnet_ids" "all" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+#############
+# RDS Aurora
+#############
 module "aurora" {
   source                          = "../../"
   name                            = "aurora-example"
   engine                          = "aurora-postgresql"
-  engine_version                  = "9.6.3"
-  subnets                         = ["${module.vpc.database_subnets}"]
-  vpc_id                          = "${module.vpc.vpc_id}"
+  engine_version                  = "10.4"
+  subnets                         = data.aws_subnet_ids.all.ids
+  vpc_id                          = data.aws_vpc.default.id
   replica_count                   = 1
   replica_scale_enabled           = true
   replica_scale_min               = 1
@@ -27,59 +31,38 @@ module "aurora" {
   instance_type                   = "db.r4.large"
   apply_immediately               = true
   skip_final_snapshot             = true
-  db_parameter_group_name         = "${aws_db_parameter_group.aurora_db_postgres96_parameter_group.id}"
-  db_cluster_parameter_group_name = "${aws_rds_cluster_parameter_group.aurora_cluster_postgres96_parameter_group.id}"
-  enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
+  db_parameter_group_name         = aws_db_parameter_group.aurora_db_postgres96_parameter_group.id
+  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.aurora_cluster_postgres96_parameter_group.id
+  //  enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
 }
 
 resource "aws_db_parameter_group" "aurora_db_postgres96_parameter_group" {
-  name        = "test-aurora-db-postgres96-parameter-group"
-  family      = "aurora-postgresql9.6"
-  description = "test-aurora-db-postgres96-parameter-group"
+  name        = "test-aurora-db-postgres10-parameter-group"
+  family      = "aurora-postgresql10"
+  description = "test-aurora-db-postgres10-parameter-group"
 }
 
 resource "aws_rds_cluster_parameter_group" "aurora_cluster_postgres96_parameter_group" {
-  name        = "test-aurora-postgres96-cluster-parameter-group"
-  family      = "aurora-postgresql9.6"
-  description = "test-aurora-postgres96-cluster-parameter-group"
+  name        = "test-aurora-postgres10-cluster-parameter-group"
+  family      = "aurora-postgresql10"
+  description = "test-aurora-postgres10-cluster-parameter-group"
 }
 
+############################
+# Example of security group
+############################
 resource "aws_security_group" "app_servers" {
   name        = "app-servers"
   description = "For application servers"
-  vpc_id      = "${module.vpc.vpc_id}"
+  vpc_id      = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "allow_access" {
   type                     = "ingress"
-  from_port                = "${module.aurora.this_rds_cluster_port}"
-  to_port                  = "${module.aurora.this_rds_cluster_port}"
+  from_port                = module.aurora.this_rds_cluster_port
+  to_port                  = module.aurora.this_rds_cluster_port
   protocol                 = "tcp"
-  source_security_group_id = "${aws_security_group.app_servers.id}"
-  security_group_id        = "${module.aurora.this_security_group_id}"
+  source_security_group_id = aws_security_group.app_servers.id
+  security_group_id        = module.aurora.this_security_group_id
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  name   = "example"
-  cidr   = "10.0.0.0/16"
-  azs    = ["${var.azs}"]
-
-  private_subnets = [
-    "10.0.1.0/24",
-    "10.0.2.0/24",
-    "10.0.3.0/25",
-  ]
-
-  public_subnets = [
-    "10.0.4.0/24",
-    "10.0.5.0/24",
-    "10.0.6.0/25",
-  ]
-
-  database_subnets = [
-    "10.0.7.0/24",
-    "10.0.8.0/24",
-    "10.0.9.0/25",
-  ]
-}
