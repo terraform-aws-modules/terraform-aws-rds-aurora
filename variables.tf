1e0f48fd416d5feca61828645fa5d62fc3fb39c8
RiceBowlJr <alexandre.kervadec@gmail.com>
2019-06-14 10:01:07 +0200

    fix(variables): missing curly bracket during merge from terraform011

--- variables.tf
+++ variables.tf
@@ -210,7 +210,8 @@ variable "replication_source_identifier" {
 variable "source_region" {
   description = "The source region for an encrypted replica DB cluster."
   default     = ""
-  
+}
+
 variable "vpc_security_group_ids" {
   description = "List of VPC security groups to associate to the cluster in addition to the SG we create in this module"
   type        = "list"
