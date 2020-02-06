9da53c29a4cc583587a4fc560d0a0bbc9e953572
shanedroid <shanek@hiringthing.com>
2020-02-06 00:27:15 -0500

    Ensure aws_iam_role and aws_iam_role_policy_attachment do not get created if create_cluster is false.

--- main.tf
+++ main.tf
@@ -123,14 +123,14 @@ data "aws_iam_policy_document" "monitoring_rds_assume_role" {
 }
 
 resource "aws_iam_role" "rds_enhanced_monitoring" {
-  count = var.monitoring_interval > 0 ? 1 : 0
+  count = var.create_cluster && var.monitoring_interval > 0 ? 1 : 0
 
   name               = "rds-enhanced-monitoring-${var.name}"
   assume_role_policy = data.aws_iam_policy_document.monitoring_rds_assume_role.json
 }
 
 resource "aws_iam_role_policy_attachment" "rds_enhanced_monitoring" {
-  count = var.monitoring_interval > 0 ? 1 : 0
+  count = var.create_cluster && var.monitoring_interval > 0 ? 1 : 0
 
   role       = local.rds_enhanced_monitoring_name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonRDSEnhancedMonitoringRole"
