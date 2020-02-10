b8cee310245050ad360c05ebcfa8b69f7965bf32
Ederson Brilhante <ederson.brilhante@grandparade.co.uk>
2020-02-10 20:39:09 +0100

    Add parameter permissions_boundary into aws_iam_role.rds_enhanced_monitoring

--- variables.tf
+++ variables.tf
@@ -307,3 +307,9 @@ variable "security_group_description" {
   type        = string
   default     = "Managed by Terraform"
 }
+
+variable "permissions_boundary" {
+  description = "The ARN of the policy that is used to set the permissions boundary for the role."
+  type        = string
+  default     = ""
+}
