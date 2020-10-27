d2df9c49c37ffedbfa81a465af64cf388f3cbdf2
Carl Allain <carl.allain@bnc.ca>
2020-10-27 18:08:35 -0400

    fix: Provide new variable to fix issue when using the module to create a secondary cluster as part of a Global Aurora database

--- variables.tf
+++ variables.tf
@@ -73,6 +73,7 @@ variable "password" {
   default     = ""
 }
 
+# This can be used in addition to username = "" to provide no credentials when creating a non-primary cluster of a Global database
 variable "is_primary_cluster" {
   description = "Set this to false if creating a non-primary cluster as part of a Global database"
   type        = bool
