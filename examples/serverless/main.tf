0390b59a279eb9a02126df29c43f5cc828d3870f
Lukas W <wojukasz@users.noreply.github.com>
2022-06-20 16:34:44 +0100

    feat: Add MySql serverless v2 example (#295)

--- examples/serverless/main.tf
+++ examples/serverless/main.tf
@@ -12,27 +12,6 @@ locals {
 }
 
 ################################################################################
-# Supporting Resources
-################################################################################
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
-
-  name = local.name
-  cidr = "10.99.0.0/18"
-
-  azs              = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  public_subnets   = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
-  private_subnets  = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
-  database_subnets = ["10.99.7.0/24", "10.99.8.0/24", "10.99.9.0/24"]
-
-  enable_nat_gateway = false # Disabled NAT to be able to run this example quicker
-
-  tags = local.tags
-}
-
-################################################################################
 # RDS Aurora Module - PostgreSQL
 ################################################################################
 
@@ -131,6 +110,58 @@ resource "aws_rds_cluster_parameter_group" "example_mysql" {
 }
 
 ################################################################################
+# RDS Aurora Module - MySQL Serverless V2
+################################################################################
+
+module "aurora_mysql_serverlessv2" {
+  source = "../../"
+
+  name              = "${local.name}-mysqlv2"
+  engine            = "aurora-mysql"
+  engine_mode       = "provisioned"
+  engine_version    = "8.0.mysql_aurora.3.02.0"
+  storage_encrypted = true
+
+  vpc_id                = module.vpc.vpc_id
+  subnets               = module.vpc.database_subnets
+  create_security_group = true
+  allowed_cidr_blocks   = module.vpc.private_subnets_cidr_blocks
+
+  monitoring_interval = 60
+
+  apply_immediately   = true
+  skip_final_snapshot = true
+
+  db_parameter_group_name         = aws_db_parameter_group.example_mysql8.id
+  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.example_mysql8.id
+
+  serverlessv2_scaling_configuration = {
+    min_capacity = 2
+    max_capacity = 10
+  }
+
+  instance_class = "db.serverless"
+  instances = {
+    one = {}
+    two = {}
+  }
+}
+
+resource "aws_db_parameter_group" "example_mysql8" {
+  name        = "${local.name}-aurora-db-mysql8-parameter-group"
+  family      = "aurora-mysql8.0"
+  description = "${local.name}-aurora-db-mysql8-parameter-group"
+  tags        = local.tags
+}
+
+resource "aws_rds_cluster_parameter_group" "example_mysql8" {
+  name        = "${local.name}-aurora-mysql8-cluster-parameter-group"
+  family      = "aurora-mysql8.0"
+  description = "${local.name}-aurora-mysql8-cluster-parameter-group"
+  tags        = local.tags
+}
+
+################################################################################
 # RDS Aurora Module - PostgreSQL Serverless V2
 ################################################################################
 
@@ -140,7 +171,8 @@ data "aws_rds_engine_version" "postgresql" {
 }
 
 module "aurora_postgresql_serverlessv2" {
-  source            = "../../"
+  source = "../../"
+
   name              = "${local.name}-postgresqlv2"
   engine            = data.aws_rds_engine_version.postgresql.engine
   engine_mode       = "provisioned"
@@ -184,4 +216,25 @@ resource "aws_rds_cluster_parameter_group" "example_postgresql13" {
   family      = "aurora-postgresql13"
   description = "${local.name}-aurora-postgres13-cluster-parameter-group"
   tags        = local.tags
+}
+
+################################################################################
+# Supporting Resources
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = local.name
+  cidr = "10.99.0.0/18"
+
+  azs              = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  public_subnets   = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
+  private_subnets  = ["10.99.3.0/24", "10.99.4.0/24", "10.99.5.0/24"]
+  database_subnets = ["10.99.7.0/24", "10.99.8.0/24", "10.99.9.0/24"]
+
+  enable_nat_gateway = false # Disabled NAT to be able to run this example quicker
+
+  tags = local.tags
 }
