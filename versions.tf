586dfa0527c4338c42d6ae5f859af9f88a1fccf8
William Garrido <3422794+FriedCircuits@users.noreply.github.com>
2022-08-03 16:59:51 -0700

    Allows AWS versions

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.60"
+      version = "~> 3.30" # Allows for modules that use the TF SG module that need <3.38.
     }
   }
   required_version = ">= 0.14"
