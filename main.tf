ae4703a920fba7d860fc2ee88fe06304a66c7738
magreenbaum <magreenbaum>
2023-02-12 10:21:28 -0500

    add conditional to prevent log group creation if using cluster name prefix

--- main.tf
+++ main.tf
@@ -442,7 +442,7 @@ resource "aws_db_parameter_group" "this" {
 ################################################################################
 
 resource "aws_cloudwatch_log_group" "this" {
-  for_each = toset([for log in var.enabled_cloudwatch_logs_exports : log if local.create_cluster && var.create_cloudwatch_log_group])
+  for_each = toset([for log in var.enabled_cloudwatch_logs_exports : log if local.create_cluster && var.create_cloudwatch_log_group && !var.cluster_use_name_prefix])
 
   name              = "/aws/rds/cluster/${var.name}/${each.value}"
   retention_in_days = var.cloudwatch_log_group_retention_in_days
