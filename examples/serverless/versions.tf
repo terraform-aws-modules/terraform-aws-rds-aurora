f2352db96c4f931178a82e181ed4639c27be4471
fdesouza-ssense <91991969+fdesouza-ssense@users.noreply.github.com>
2025-01-30 12:09:35 -0500

    Revert "Update aurora"

--- examples/serverless/versions.tf
+++ examples/serverless/versions.tf
@@ -1,14 +1,10 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 0.13"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.81"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.5"
+      version = ">= 3.63"
     }
   }
 }
